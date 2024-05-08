c45473533ad6f8a61ff51be78f0cb49ad3352839
GustavoAndo <gustavo.k.ando@gmail.com>
2024-05-08 15:08:20 -0300

    Adicionando conexão do banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import express from 'express'
 import 'dotenv/config'
-import cors from 'cors';
+import cors from 'cors'
+import { AppDataSource } from './data-source'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+AppDataSource
+    .initialize()
+    .then(() => {
+        console.log("Banco de dados conectado!")
+    })
+    .catch((e) => {
+        console.error("Erro na conxeão do banco de dados:", e)
+    })
+
 
 app.get('/', (req, res) => {
     res.send('Test')
---
+++ src/data-source.ts
@@ -0,0 +1,19 @@
+import { DataSource } from "typeorm"
+import "reflect-metadata"
+import 'dotenv/config'
+
+const DB_PORT = process.env.DB_PORT as number | undefined
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    logging: false,
+    entities: [],
+    migrations: [],
+    subscribers: [],
+})
